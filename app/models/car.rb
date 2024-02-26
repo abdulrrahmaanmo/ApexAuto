90ef40aea9117d6da03075cbdbd912f8b8c93060
Jamal Abubakari <jamalabubalari@gmail.com>
2024-02-26 16:19:50 +0000

    car validations and associations added

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,11 @@
 class Car < ApplicationRecord
+  has_many :bookings
+  validates  :color, presence: true
+  validates  :brand, presence: true
+  validates  :fuel_type, presence: true
+  validates  :bluetooth, inclusion: { in: [true, false] }
+  validates  :automatic, inclusion: { in: [true, false] }
+  validates  :price_per_day, presence: true
+  validates  :seats, presence: true
+  validates :seats, numericality: { only_integer: true }
 end
