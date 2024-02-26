273fc36c0325d5bd8003f3d6ba8881db82fad4ea
AbdulRrahmaan Omar <abdulrrahmaanwork@gmail.com>
2024-02-26 15:40:11 +0000

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,19 @@
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
+
+puts "Cleaning DataBase"
+Car.destroy_all
+puts "Creating Cars"
+
+Car.create(price_per_day: 77.50, brand: "Audi", seats: 5, color: "White", automatic: false, bluetooth: true, fuel_type: "Petrol")
+Car.create(price_per_day: 121.75, brand: "BMW", seats: 7, color: "Blue", automatic: true, bluetooth: true, fuel_type: "Petrol")
+Car.create(price_per_day: 100.00, brand: "BMW", seats: 5, color: "Black", automatic: false, bluetooth: true, fuel_type: "Petrol")
+Car.create(price_per_day: 99.99, brand: "Audi", seats: 5, color: "White", automatic: true, bluetooth: false, fuel_type: "Diesel")
+Car.create(price_per_day: 250.50, brand: "Lamborghini", seats: 2, color: "Yellow", automatic: true, bluetooth: true, fuel_type: "Petrol")
+Car.create(price_per_day: 175.10, brand: "Mercedes", seats: 5, color: "Black", automatic: true, bluetooth: true, fuel_type: "Diesel")
+Car.create(price_per_day: 225.25, brand: "Ferrari", seats: 2, color: "Red", automatic: true, bluetooth: true, fuel_type: "Petrol")
+
+puts "Finished"
+
 #   end
