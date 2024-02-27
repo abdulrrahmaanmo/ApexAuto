a6040ef5e8ba8c1d3f6cd9475e7d9c35cb5d8823
AbdulRrahmaan Omar <abdulrrahmaanwork@gmail.com>
2024-02-27 11:43:57 +0000

    Created form for new cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,23 @@ class CarsController < ApplicationController
   def index
     @cars = Car.all
   end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save!
+      redirect_to car_path(@car)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :color, :fuel_type, :price_per_day, :automatic, :bluetooth, :seats)
+  end
 end
